8bdd05ec7f49e6cb0f0067deb428ce67aed48d07
jefti <jeftimeira@gmail.com>
2023-04-14 00:26:28 -0300

    implementado o login e a pega das informações

--- scripts/script.js
+++ scripts/script.js
@@ -1,1 +1,29 @@
-alert('conectado');
+axios.defaults.headers.common['Authorization'] = 'dVY1sNGArXuoHRdyAyWGnZGb';
+
+
+const user = {name: prompt('informe o seu nome por favor:')};
+const requisicao = axios.post('https://mock-api.driven.com.br/api/vm/uol/participants',user);
+requisicao.then(resposta);
+const senha = setInterval(confirmarlogin, 5000);
+
+
+function deslogar(){
+    clearInterval(senha);
+}
+
+function confirmarlogin(){
+    const confirm = axios.post('https://mock-api.driven.com.br/api/vm/uol/status',user);
+}
+
+function resposta(response){
+    console.log(`Servidor enviou resposta: ${response}`);
+}
+
+function pegarMensagens(){
+    pedido_msg = axios.get('https://mock-api.driven.com.br/api/vm/uol/messages');
+    pedido_msg.then(exibirmensagens);
+}
+function exibirmensagens(response){
+    const Lista = response;
+    console.log(Lista);
+}

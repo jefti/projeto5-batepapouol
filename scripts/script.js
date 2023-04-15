126c628adc3f076102c1988f19566bdf9e818bf3
jefti <jeftimeira@gmail.com>
2023-04-15 16:23:18 -0300

    versão finalizada sem o bônus

--- scripts/script.js
+++ scripts/script.js
@@ -1,13 +1,34 @@
+//1. CAdastramos as informações necessárias para contatar o Servidor:
 axios.defaults.headers.common['Authorization'] = 'dVY1sNGArXuoHRdyAyWGnZGb';
 
-
+//2 O usuario tenta logar com seu proprio nome
 const user = {name: prompt('informe o seu nome por favor:')};
 const requisicao = axios.post('https://mock-api.driven.com.br/api/vm/uol/participants',user);
 requisicao.then(resposta);
+requisicao.catch(outroNome);
+
+//3 manter o contato com o servidor para confirmar status e resetar tela
 const senha = setInterval(confirmarlogin, 5000);
-const chavesMensagens = setInterval(pegarMensagens, 1000);
+const chavesMensagens = setInterval(pegarMensagens, 3000);
+
 let objeto_teste = {} ;
 let lista_teste = {};
+
+
+function outroNome(){
+    user.name = prompt('O primeiro nome falhou, insira outro nome:');
+    const requisicao2 = axios.post('https://mock-api.driven.com.br/api/vm/uol/participants',user);
+    requisicao2.then(resposta);
+    requisicao2.catch(outroNome);
+
+    // o usuario insere o nome
+    // tentamos conectar no servidor com esse nome
+    // se verdedeiro o código continua
+    //se falso voltamos ao começo
+}
+
+
+
 
 document.addEventListener("keypress", function(e){
     if(e.key == "Enter"){
@@ -47,10 +68,13 @@ function exibirmensagens(response){
     const tela = document.querySelector('.conteudo');
     Limpartela();
 
-    for(let cont = (Lista.length-1); cont>=0; cont--){
+    for(let cont = 0; cont < Lista.length; cont++){
         tela.innerHTML += escreverMensagem(Lista[cont]);
     }
     //console.log('Mensagens atualizadas ...');
+    let Lista_mensagens = document.querySelectorAll('.mensagem-box');
+    let primeiro = Lista_mensagens[(Lista_mensagens.length -1)];
+    primeiro.scrollIntoView();
 }
 
 function Limpartela(){
@@ -66,13 +90,21 @@ function escreverMensagem(objeto){
     const hora = objeto.time;
     let texto = '';
     if(tipo == "status"){
-        texto = `<div class='mensagem-box login'><div class='mensagem'><span class='horario'>(${hora})</span><span> <strong class='negrito'> ${remetente} </strong> ${mensagem} </span> </div></div>`;
+        texto = `<div data-test="message" class='mensagem-box login'><div class='mensagem'><span class='horario'>(${hora})</span><span> <strong class='negrito'> ${remetente} </strong> ${mensagem} </span> </div></div>`;
     } else if(tipo =='message'){
-        texto = `<div class="mensagem-box">
+        texto = `<div data-test="message" class="mensagem-box">
         <div class="mensagem">
           <span class="horario">(${hora})</span><span> <strong class="negrito"> ${remetente}</strong> para <strong class="negrito">${destino} </strong>: ${mensagem}</span> 
         </div>
       </div>`;
+    } else if(tipo == 'private_message'){
+        texto = `
+        <div data-test="message" class="mensagem-box reservada">
+        <div class="mensagem">
+          <span class="horario">(${hora})</span><span> <strong class="negrito"> ${remetente}</strong> reservadamente para <strong class="negrito">${destino} </strong>: ${mensagem}</span> 
+        </div>
+      </div>
+        `
     }
     return texto;
 }
@@ -87,13 +119,18 @@ let carta = {
 
     let promessas1 = axios.post('https://mock-api.driven.com.br/api/vm/uol/messages',carta);
     promessas1.then(fale);
-    promessas1.catch(fale);
+    promessas1.catch(falhaMensagem);
 }
 
 
 function fale(resposta){
     console.log(resposta);
     pegarMensagens();
+}
+
+function falhaMensagem(){
+    alert('Houve um problema no servidor que desligou o usuario, por favor refaça o login');
+    window.location.reload();
 }
 
 function enviarMensagem(){
